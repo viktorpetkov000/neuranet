45c192aeb78073fd02ad08c3a8730599a9964998
viktorpetkov000 <viktor.petkov000@gmail.com>
2022-01-07 15:50:56 +0200

    first commit

---
+++ data/data2.js
@@ -0,0 +1,23 @@
+var topNodes = [
+    { "name":"Top 40",
+    "links": [
+        {"id":0, "weight":"-0.475"},
+        {"id":1, "weight":"-0.588"},
+        {"id":2, "weight":"0.339"},
+    ]},
+    { "name":"Top 10",
+    "links": [
+        {"id":0, "weight":"-0.475"},
+    ]},
+    { "name":"Top 40",
+    "links": [
+        {"id":0, "weight":"-0.475"},
+        {"id":1, "weight":"-0.588"},
+    ]},
+]
+
+var midNodes = [
+    { "name":"Mid 50", },
+    { "name":"Mid 20", },
+    { "name":"Mid 10", },
+]
